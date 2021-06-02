7752ef6f528f0e078934cd5d0eb94c29d06b8059
SincerelyBrittany <sincerelybr@gmail.com>
2021-06-01 22:26:32 -0400

    allows user to save entries

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let countEl = document.getElementById("count-el");
 
-console.log(countEl);
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
@@ -12,4 +12,13 @@ function increment() {
 function decrement() {
   count = count - 1;
   countEl.innerText = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+  // 3. Render the variable in the saveEl using innerText
+  saveEl.innerText += countStr;
+  // NB: Make sure to not delete the existing content of the paragraph
+  console.log(count);
 }
